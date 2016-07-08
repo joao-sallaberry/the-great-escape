3aa71e51c222f3b53c5f22aea79894309ed02799
Joao Sallaberry <joaovictorhs@gmail.com>
2016-07-07 23:26:06 -0300

    car image and oil obstacle

--- game.js
+++ game.js
@@ -30,6 +30,7 @@ Game.initialize = function() {
   this.player.H = 100
   this.player.x = (this.SCREEN_WIDTH - this.player.W) / 2
   this.player.y = this.SCREEN_HEIGHT - this.player.H - 50
+  this.player.isSlidingTimer = 0
 
   // car control
   this.isLeftKeyPressed = false
@@ -50,6 +51,15 @@ Game.initialize = function() {
   // obstacles
   this.obstacles = []
   this.obsTimeCounter = 0
+
+  this.obsCar = {}
+  this.obsCar.img = new Image()
+  this.obsCar.img.src = './assets/obs_car1.png'
+  this.obsCar.W = 50
+  this.obsCar.H = 100
+
+  this.obsOil = {}
+  this.obsOil.R = 30
 
   // explosion
   this.explosion = {}
@@ -73,24 +83,47 @@ Game.initialize = function() {
 Game.draw = function() {
   var rect = this.context.clearRect(0, 0, this.SCREEN_WIDTH, this.SCREEN_HEIGHT);
 
-  // var ctx = this.context
-  // this.road.onload = function() {
-  //   console.log('imagem carregou')
-  // };
-
   // draw road
   for (var i = this.roadSection.length - 1; i >= 0; i--)
     this.context.drawImage(this.road, this.roadSection[i].x, this.roadSection[i].y)
 
   // draw car
   this.context.drawImage(this.player.img, this.player.x, this.player.y, this.player.W, this.player.H)
+  if (this.player.isSlidingTimer) {
+    makeLine(true)
+    makeLine(false)
+    function makeLine(leftWheel) {
+      Game.context.beginPath();
+      var x = Game.player.x + (leftWheel ? 15 : (Game.player.W - 15))
+      var y = Game.player.y + Game.player.H + 10
+      Game.context.moveTo(x, y);
+      var x2 = x + (Game.player.slideSide ? -15 : 15)
+      Game.context.lineTo(x2, y + 60);
+      Game.context.lineWidth = 10;
+      Game.context.stroke();
+    }
+  }
 
   // draw obstacles
   for (var i = this.obstacles.length - 1; i >= 0; i--) {
-    this.context.fillRect(
-      this.obstacles[i].x, this.obstacles[i].y,
-      this.obstacles[i].W, this.obstacles[i].H
-    )
+    switch (this.obstacles[i].type) {
+      case 'car':
+        this.context.drawImage(
+          this.obsCar.img,
+          this.obstacles[i].x, this.obstacles[i].y,
+          this.obstacles[i].W, this.obstacles[i].H
+        )
+        break;
+      case 'oil':
+        this.context.beginPath();
+        this.context.arc(this.obstacles[i].x, this.obstacles[i].y, this.obstacles[i].R, 0, 2 * Math.PI, false);
+        this.context.fillStyle = 'gray';
+        this.context.fill();
+        this.context.lineWidth = 5;
+        this.context.strokeStyle = '#000000';
+        this.context.stroke();
+        break;
+    }
   }
 
   // draw explosion
@@ -114,18 +147,33 @@ Game.update = function() {
       this.roadSection[i].y = - this.SCREEN_HEIGHT
   }
 
-  // moves car
-  var CAR_SIDE_SPEED = 5
+  // moves player
+  var PLAYER_SIDE_SPEED = 5
   
-  if (this.isLeftKeyPressed)
-    this.player.x -= CAR_SIDE_SPEED
-  if (this.isRightKeyPressed)
-    this.player.x += CAR_SIDE_SPEED
+  if (this.player.isSlidingTimer) {
+    this.player.isSlidingTimer--
 
-  if (this.player.x < this.LEFT_BOUNDARY)
-    this.player.x = this.LEFT_BOUNDARY
-  if (this.player.x > this.RIGHT_BOUNDARY - this.player.W)
-    this.player.x = this.RIGHT_BOUNDARY - this.player.W
+    if (this.player.slideSide == 0)
+      this.player.x -= PLAYER_SIDE_SPEED / 2
+    else
+      this.player.x += PLAYER_SIDE_SPEED / 2
+
+    checkBoundaries()
+  }
+  else {
+    if (this.isLeftKeyPressed)
+      this.player.x -= PLAYER_SIDE_SPEED
+    if (this.isRightKeyPressed)
+      this.player.x += PLAYER_SIDE_SPEED 
+    checkBoundaries()
+  }
+
+  function checkBoundaries() {
+    if (Game.player.x < Game.LEFT_BOUNDARY)
+      Game.player.x = Game.LEFT_BOUNDARY
+    if (Game.player.x > Game.RIGHT_BOUNDARY - Game.player.W)
+      Game.player.x = Game.RIGHT_BOUNDARY - Game.player.W
+  }
 
   // create obstacles
   this.obsTimeCounter++
@@ -135,26 +183,66 @@ Game.update = function() {
   }
 
   function newObstacle() {
-    var h = 80, w = 30
-    Game.obstacles.push({
-      W: w,
-      H: h,
+    var type = Math.floor((Math.random() * 2))
+
+    if (type == 0)
+      Game.obstacles.push(newCarObstacle())
+    else if (type = 1)
+      Game.obstacles.push(newOilObstacle())
+    else
+      console.error('unknown obstacle type')
+  }
+
+  function newCarObstacle() {
+    return {
+      type: 'car',
+      W: Game.obsCar.W,
+      H: Game.obsCar.H,
       x: Math.floor(
-          (Math.random() * (Game.RIGHT_BOUNDARY - Game.LEFT_BOUNDARY - w))
+          (Math.random() * (Game.RIGHT_BOUNDARY - Game.LEFT_BOUNDARY - Game.obsCar.W))
           + Game.LEFT_BOUNDARY
       ),
-      y: -h
-    })
+      y: -Game.obsCar.H
+    }
+  }
+
+  function newOilObstacle() {
+    return {
+      type: 'oil',
+      R: Game.obsOil.R,
+      x: Math.floor(
+          (Math.random() * (Game.RIGHT_BOUNDARY - Game.LEFT_BOUNDARY - Game.obsOil.R))
+          + Game.LEFT_BOUNDARY
+      ),
+      y: -Game.obsOil.R
+    }
   }
 
   // move obstacles
-  for (var i = this.obstacles.length - 1; i >= 0; i--)
-    this.obstacles[i].y += ROAD_SPEED
+  for (var i = this.obstacles.length - 1; i >= 0; i--) {
+    switch (this.obstacles[i].type) {
+      case 'car':
+        this.obstacles[i].y += ROAD_SPEED / 2
+        break;
+      default:
+        this.obstacles[i].y += ROAD_SPEED
+    }
+  }
+
   this.obstacles = this.obstacles.filter((obs) => { 
     return obs.y < Game.SCREEN_HEIGHT
   })
 
   // detect collision
+  function collided(obs) {
+    switch (obs.type) {
+      case 'car':
+        return rectCollision(obs)
+        break;
+      default:
+        return circleCollision(obs)
+    }
+  }
   function rectCollision(obs) {
     return !(obs.x > (Game.player.x + Game.player.W) || 
              (obs.x + obs.W) < Game.player.x || 
@@ -162,21 +250,44 @@ Game.update = function() {
              (obs.y + obs.H) < Game.player.y
             )
   }
+  // http://stackoverflow.com/questions/20885297/collision-detection-in-html5-canvas
+  function circleCollision(obs) {
+    var distX = Math.abs(obs.x - Game.player.x-Game.player.W/2)
+    var distY = Math.abs(obs.y - Game.player.y-Game.player.H/2)
 
-  if (this.explosion.started) {
-    this.explosion.timeOut++
-    if (this.explosion.timeOut >= this.fps / 15) {
-      this.explosion.currSprite++
-      if (this.explosion.currSprite == this.explosion.ROWS * this.explosion.COLS - 1)
-        Game.initialize()
-      this.explosion.timeOut = 0
-    }
+    if (distX > (Game.player.W/2 + obs.R)) { return false }
+    if (distY > (Game.player.H/2 + obs.R)) { return false }
+
+    if (distX <= (Game.player.W/2)) { return true } 
+    if (distY <= (Game.player.H/2)) { return true }
+
+    var dx=distX-Game.player.W/2
+    var dy=distY-Game.player.H/2
+    return (dx*dx+dy*dy<=(obs.R*obs.R))
   }
-  else {
+
+  if (!this.explosion.started) {
     for (var i = this.obstacles.length - 1; i >= 0; i--) {
-      if (rectCollision(this.obstacles[i]))
-        this.explosion.started = true
+      switch (this.obstacles[i].type) {
+        case 'car':
+          if(rectCollision(this.obstacles[i]))
+            this.explosion.started = true
+          break;
+        case 'oil':
+          if (!this.player.isSlidingTimer && circleCollision(this.obstacles[i]))
+            this.player.isSlidingTimer = 100
+            this.player.slideSide = Math.floor((Math.random() * 2))
+          break;
+      }
     }
   }
+  else { // game over
+    if (this.explosion.timeOut++ >= this.fps / 15) {
+      this.explosion.timeOut = 0
+      if (this.explosion.currSprite++ == this.explosion.ROWS * this.explosion.COLS - 1)
+        Game.initialize()
+    }
+  }
+  
 
 };
